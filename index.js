3e4ed5254c6dafac2b08bf7d0e20f5cf62d70cb2
Delton Ding <dsh0416@gmail.com>
2017-07-06 01:16:40 +0800

    Simplify implementation

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 "use strict";
 
 function tryToParseJson(string, reviver) {
-  var result;
-
   try {
-    result = JSON.parse(string, reviver);
+    return JSON.parse(string, reviver);
   } catch (error) {
     // oh error? well just return undefined, no biggie
   }
-
-  return result;
 }
 
 module.exports = tryToParseJson;
